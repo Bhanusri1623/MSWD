79c532741ee36c24f884a6de8816adc98cfa2725
Bhanusri1623 <74002349+Bhanusri1623@users.noreply.github.com>
2021-06-08 12:13:36 +0530

    Add files via upload

---
+++ part1/1.8/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+
+const Statistics = (props) => {
+	return (
+		<div>
+			<h3>Good Feedback: {props.good}</h3>
+			<h3>Neutral Feedback: {props.neutral}</h3>
+			<h3>Bad Feedback: {props.bad}</h3>
+			<h3>All Feedbacks: {props.All}</h3>
+			<h3>Average: {props.average}</h3>
+			<h3>Positive: {props.positive}</h3>
+		</div>
+	)
+}
+
+const App = () => {
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+	
+	return (
+		<div>
+			<h1>Please Give Feedback this will help us, Thank you!</h1><br/>
+			<button onClick = {() => setGood(good + 1)}>Good</button>
+			<button onClick = {() => setNeutral(neutral + 1)}>Neutral</button>
+			<button onClick = {() => setBad(bad + 1)}>Bad</button>
+			<br/>
+			<h1>Statistics</h1>
+			<br/>
+			<Statistics 
+				good={good} 
+				neutral= {neutral} 
+				bad= {bad} 
+				All= {good + neutral + bad} 
+				average= {(good - bad)/(good + neutral + bad)} 
+				positive= {(good + neutral)/(good + neutral + bad)}
+			/>
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+export default App;
